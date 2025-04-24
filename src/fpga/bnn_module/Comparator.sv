f4138a57d3991608609c4059efe49bdfe2185731
cameronbed <cameronobedard@gmail.com>
2025-04-24 13:32:23 -0400

    integrating sequential bnn_module

--- src/fpga/bnn_module/Comparator.sv
+++ src/fpga/bnn_module/Comparator.sv
@@ -8,22 +8,36 @@ module Comparator #(
     parameter int IC = 10,
     parameter int OUTPUT_BIT = $clog2(IC + 1)  // num of bits to enumerate each class
 ) (
+    input logic clk,
+    input logic data_in_ready,
     input logic signed [15:0] in[0:IC-1],
-    output [OUTPUT_BIT-1:0] out
+    output logic [OUTPUT_BIT-1:0] out,
+    output logic data_out_ready
 );
   logic signed [15:0] max;
   logic [OUTPUT_BIT-1:0] max_ind;
-  always_comb begin
-    max = in[0];
-    max_ind = 0;
-    for (int ic = 1; ic < IC; ic = ic + 1) begin
-      if (in[ic] > max) begin
-        max = in[ic];
-        max_ind = ic[OUTPUT_BIT-1:0];
+  integer cur_ic;
+
+  always_ff @(posedge clk) begin
+    if (!data_in_ready) begin
+      max <= in[0];
+      max_ind <= 0;
+      cur_ic <= 0;
+      data_out_ready <= 0;
+      out <= 0;
+    end else
+    if (data_out_ready) begin
+    end else begin
+      if (max < in[cur_ic]) begin
+        max <= in[cur_ic];
+        max_ind <= cur_ic[OUTPUT_BIT-1:0];
+      end
+      cur_ic <= cur_ic + 1;
+      if (cur_ic == IC) begin
+        data_out_ready <= 1;
+        out <= max_ind;
       end
     end
   end
-  assign out = max_ind;  // return the index with largest value
 
 endmodule
-
--- src/fpga/bnn_module/MaxPool2d.sv
+++ src/fpga/bnn_module/MaxPool2d.sv
@@ -1,25 +1,46 @@
-`include "bnn_module/MaxPoolCore.sv"
+`include "MaxPoolCore.sv"
 
 module MaxPool2d #(
     parameter int IMG_IN_SIZE = 28,
     parameter int IMG_OUT_SIZE = IMG_IN_SIZE / 2,
     parameter int IC = 10
 ) (
-    input  logic [  IMG_IN_SIZE*IMG_IN_SIZE-1:0] img_in [0:IC-1],
-    output logic [IMG_OUT_SIZE*IMG_OUT_SIZE-1:0] img_out[0:IC-1]
+    input logic clk,
+    input logic data_in_ready,
+    input logic [IMG_IN_SIZE*IMG_IN_SIZE-1:0] img_in[0:IC-1],
+    output logic [IMG_OUT_SIZE*IMG_OUT_SIZE-1:0] img_out[0:IC-1],
+    output logic data_out_ready
 );
 
-  genvar ic;
-  generate
-    for (ic = 0; ic < IC; ic = ic + 1) begin : maxpool_core_gen
-      MaxPoolCore #(
-          .IMG_IN_SIZE(IMG_IN_SIZE)
-      ) maxpool (
-          .img_in (img_in[ic]),
-          .img_out(img_out[ic])
-      );
+  integer cur_oc;
+  logic [IMG_IN_SIZE*IMG_IN_SIZE-1:0] core_img_in;
+  logic [IMG_OUT_SIZE*IMG_OUT_SIZE-1:0] core_img_out;
+
+  always_ff @(posedge clk) begin
+    if (!data_in_ready) begin
+      cur_oc <= 0;
+      data_out_ready <= 0;
+      core_img_in <= img_in[0];
+      for (int i = 0; i < IC; i = i + 1) begin
+        img_out[i] <= 0;
+      end
+    end else
+    if (data_out_ready) begin
+    end else begin
+      core_img_in <= img_in[cur_oc+1];
+      img_out[cur_oc] <= core_img_out;
+      cur_oc <= cur_oc + 1;
+      if (cur_oc == IC - 1) begin
+        data_out_ready <= 1;
+      end
     end
-  endgenerate
+  end
+
+  MaxPoolCore #(
+      .IMG_IN_SIZE(IMG_IN_SIZE)
+  ) maxpool (
+      .img_in (core_img_in),
+      .img_out(core_img_out)
+  );
 
 endmodule
-
