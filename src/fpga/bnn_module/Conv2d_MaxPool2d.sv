7fec786a82a999711c47b1eeec08ad7922ab498b
CosmoJX <hx2227@nyu.edu>
2025-05-01 12:16:14 -0400

    updated Conv2d_MaxPool2d.sv and ConvCore.sv

--- src/fpga/bnn_module/Conv2d_MaxPool2d.sv
+++ src/fpga/bnn_module/Conv2d_MaxPool2d.sv
@@ -18,12 +18,15 @@ module Conv2d_MaxPool2d #(
     logic [IC*9-1:0] core_weight;
     logic [CONV_IMG_OUT_SIZE*CONV_IMG_OUT_SIZE-1:0] core_img_out;
     logic [POOL_IMG_OUT_SIZE*POOL_IMG_OUT_SIZE-1:0] pool_img_out;
+    logic core_data_in_ready;
+    logic core_data_out_ready;
     integer cur_oc;
 
     always_ff @( posedge clk ) begin : ConvBlock
         if (!data_in_ready) begin
             cur_oc <= 0;
             data_out_ready <= 0;
+            core_data_in_ready <= 0;
             core_weight <= weights[0];
             for (int i=0; i<OC; i=i+1) begin
                 img_out[i] <= 0;
@@ -31,11 +34,19 @@ module Conv2d_MaxPool2d #(
         end
         else if (data_out_ready) begin end
         else begin
-            core_weight <= weights[cur_oc+1];
-            img_out[cur_oc] <= pool_img_out;
-            cur_oc <= cur_oc + 1;
-            if (cur_oc == OC-1) begin
-                data_out_ready <= 1;
+            if (core_data_out_ready) begin
+                core_data_in_ready <= 0;
+                if (cur_oc == OC) begin
+                    data_out_ready <= 1;
+                end
+                else begin
+                    cur_oc <= cur_oc + 1;
+                    core_weight <= weights[cur_oc+1];
+                    img_out[cur_oc] <= pool_img_out;
+                end
+            end
+            else begin
+                core_data_in_ready <= 1;
             end
         end
     end
@@ -44,9 +55,12 @@ module Conv2d_MaxPool2d #(
         .IC(IC),
         .IMG_IN_SIZE(CONV_IMG_IN_SIZE)
     ) core (
+        .clk(clk),
+        .data_in_ready(core_data_in_ready),
         .img_in(img_in),
         .weights(core_weight),
-        .img_out(core_img_out)
+        .img_out(core_img_out),
+        .data_out_ready(core_data_out_ready)
     );
 
     MaxPoolCore#(
